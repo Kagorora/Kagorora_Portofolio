a2aa6926c980edc3e181fed696e8fa87c39e1d16
Kagorora <kagororamaxime@gmail.com>
2020-06-15 22:48:46 +0200

    ft(information): allow users to view my information
    - Allow Users to view my information
    
    [Finishes #173341770]

---
+++ src/component/personalnfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import maxime from "../style/images/maxime.png";
+
+const personalInfo = () => {
+  return (
+    <div id="PersonalInfo">
+      <div className="bio">
+        <span className="DescriptionTitle">
+          <p className="UIheader">UI/UX Fronted Dev</p>
+        </span>
+        <p className="UIdescription">
+          <span id="maximeName">Kagorora</span> Enjoys building beautiful user
+          interface that are easy to interact with and he is always looking for
+          user satisfaction while experiencing his work. Don't hesitate to reach
+          out, We can make your clients more pleased with your products!
+        </p>
+      </div>
+      <div className="bio2">
+        <span className="DescriptionTitle2">
+          <p className="BACKENDHeader">Programmer / Backend</p>
+        </span>
+        <p className="BACKENDdescription">I am always open to work on meaningful project
+        either independent or open source projects that affect positively people's lives.
+
+        If interested with work with me, please feel free to reach out!
+        </p>
+      </div>
+      <div className="imageSide">
+        <div className="image">
+          <div className="element" id="element"></div>
+          
+          <img src={maxime} alt="user profile" className="avatar--small" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default personalInfo;
